35267cfccb3e42feb4cdb14d72b61277d4785d8c
Kevin <i19045@hb.dhbw-stuttgart.de>
2022-05-12 11:54:24 +0200

    Added py to exe logic
    Added py to exe logic

--- Controller/FrontendHelper.py
+++ Controller/FrontendHelper.py
@@ -1,17 +1,25 @@
 from PyQt5 import QtWidgets
+from Build.build import resource_path
 import json
 
-TITLES_FILE = "Models/titles.txt"
+TITLES_FILE = resource_path("Models/titles.txt")
+EXTRAS_FILE = resource_path("Models/name_extras.txt")
+FEMALE_FILE = resource_path("Models/female_salutations.txt")
+MALE_FILE = resource_path("Models/male_salutations.txt")
+LETTER_ADDRESS_FILE = resource_path("Models/letter_address.json")
+
+"""TITLES_FILE = "Models/titles.txt"
 EXTRAS_FILE = "Models/name_extras.txt"
 FEMALE_FILE = "Models/female_salutations.txt"
 MALE_FILE = "Models/male_salutations.txt"
-LETTER_ADDRESS_FILE = "Models/letter_address.json"
+LETTER_ADDRESS_FILE = "Models/letter_address.json"""""
 
 def load_json(file):
     with open(file, encoding='utf-8') as f:
         address_dict = json.load(f)
     return address_dict
 
+# Load .txt into list for split and gui
 def load_list(file):
     list = []
     with open(file) as f:
@@ -19,11 +27,12 @@ def load_list(file):
             list.append(line.replace('\n', ''))
     return list
 
-
+# Save list to .txt for gui
 def save_list(output, file):
     with open(file, 'w') as f:
         f.writelines(output)
 
+# Load list into corresponding gui dialog
 def load_data(dialog, file):
     data = load_list(file)
     dialog.tableWidget.setRowCount(len(data))
@@ -35,6 +44,7 @@ def load_data(dialog, file):
         else:
             dialog.tableWidget.removeRow(row)
 
+# Save new list of gui dialog into .txt
 def save_data(dialog, file):
     new_data = []
     rows = dialog.tableWidget.rowCount()
